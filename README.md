c18bb89e7c61aebe2727e50069f02eb742faff3f
Pierre Brengard <pierre@brengard.fr>
2018-01-24 15:18:41 +0000

    Add README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+Start application:
+
+    mongod --bind_ip=$IP --nojournal
+    webpack -d
+    node server/server.js
--- server/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  session_secret: "ilovecats",
-  mongo_url: 'mongodb://localhost:27017',
-  dbName: 'goclimbing',
-  google:{"client_id":"260234280781-os08m5koh5letslrivv2dhopel7dtkvb.apps.googleusercontent.com","project_id":"go-climbing-193017","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://accounts.google.com/o/oauth2/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"bIxqhyDYeoL_6KNQJU-zpxs1","redirect_uris":["https://go-climbing-pbrengard-1.c9users.io/auth/google/callback"],"javascript_origins":["https://go-climbing-pbrengard-1.c9users.io"]},
-}
