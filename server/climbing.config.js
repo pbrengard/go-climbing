ef78c2e151f22e7a56a13e14869990c109bd85e3
Pierre Brengard <pierre@brengard.fr>
2018-01-26 23:45:39 +0000

    Added base config file

---
+++ server/climbing.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  difficulties: [ // from easiest to most difficult
+    {id: 1, name: 'Novice', color: 'purple[200]'},
+    {id: 2, name: 'Débutant', color: 'yellow[400]'},
+    {id: 3, name: 'Débutant', color: 'orange[300]'},
+    {id: 4, name: 'Performant', color: 'green[300]'},
+    {id: 5, name: 'Confirmé', color: 'indigo[400]'},
+  ]
+  
+  walls: [
+    {id: 1,  name: "Vertical 1"},
+    {id: 2,  name: "Diamant"},
+    {id: 3,  name: "Proue"},
+    {id: 4,  name: "Ventre 1"},
+    {id: 5,  name: "Ventre 2"},
+    {id: 6,  name: "Grand dévers"},
+    {id: 7,  name: "Nez"},
+    {id: 8,  name: "Dévers"},
+    {id: 9,  name: "Réta 1"},
+    {id: 10, name: "Dalle"},
+    {id: 11, name: "Vertical 2"},
+    {id: 12, name: "Petit dévers"},
+    {id: 13, name: "Réta 2"},
+    {id: 14, name: "Coup de sabre"},
+    {id: 15, name: "Grotte"},
+    {id: 16, name: "Tunnel"},
+  ],
+};
